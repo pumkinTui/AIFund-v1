cc2824acfc3acf9f909e56b0bce29c83f1997784
pumkinTui <work.jh.shen@outlook.com>
2026-05-02 11:57:33 +0800

    entity，三层架构，工具类，异常等的基础配置

---
+++ src/main/java/com/fund/assistant/FundValuationAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.fund.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundValuationAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundValuationAssistantApplication.class, args);
+    }
+
+}
